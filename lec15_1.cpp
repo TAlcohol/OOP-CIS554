db518410021fe5369151a786f54f8196a2374ecb
TAlcohol <chun.xie2000@gmail.com>
2019-10-16 17:36:23 -0400

    midterm review

---
+++ lec15_1.cpp
@@ -0,0 +1,15 @@
+// no functor will cover in midterm test
+
+// midterm problems:
+
+// 5mins earlier
+// pen
+
+// one DB_conversion, maybe 3 level
+
+// one linked list problem
+// such as reverse, sort, removeOne/removeAll...
+
+// one problem
+// give you a class
+// complete these thing: copy, move constructor, L,R-value assignment, overloading printing, destructor, initializer_list
